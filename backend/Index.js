acb6905a7f99299bcff1b03c2cdba8895773decc
Bhawishya Singh <102045314+Bhawishya509@users.noreply.github.com>
2022-09-10 15:29:35 +0530

    first

--- backend/Index.js
+++ backend/Index.js
@@ -1,16 +1,30 @@
-import express, { urlencoded } from "express";
+import express,{urlencoded}  from "express";
 import cors from "cors"
-const app = express();
-app.use(express.json());
+const app=express()
+import multer from "multer"
+app.use(express.urlencoded({extended:false}))
 app.use(cors())
-app.use(express.urlencoded({ extended: true}));
-app.get("/", (req, res) => {
-  res.send({
-    name: "Bhawishya",
-  });
-});
-app.post("/kuku", (req, res) => {
- console.log(req.body)
- res.send("ok")
-});
-app.listen(8000);
+app.use(express.json())
+app.use("/image",express.static ("image"))
+app.get("/",(req,res)=>
+{
+  res.send("working")
+})
+let b;
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'image')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null,uniqueSuffix+""+file.originalname )
+    b=uniqueSuffix+""+file.originalname;
+  }
+})
+
+const upload = multer({ storage: storage })
+app.post("/kuku",upload.single('values'),(req,res)=>
+{
+  res.send(b)
+})
+app.listen(8000)
--- client/src/App.js
+++ client/src/App.js
@@ -2,33 +2,57 @@ import React,{useState,useEffect} from 'react'
 import axios from "axios"
 import "./App.css"
 const App = () => {
-  const[print,update]=useState("hiii")
-  const kuku=()=>
+  const[data,updatedata]=useState({
+    image:"",
+    getting:""
+  })
+
+  const check=(e)=>
+  
   {
-    axios
-    .post("http://localhost:8000/kuku", {
-      firstname: "Bhawishya singh",
-    })
+    e.preventDefault()
+     const fromdata=new FormData();
+    fromdata.append("values",data.image)
+    axios.post('http://localhost:8000/kuku',fromdata)
     .then(function (response) {
-      update(response.data)
+      updatedata((datas)=>
+      {
+        let c=`http://localhost:8000/image/${response.data}`
+         datas.getting=c;
+         return {...datas}
+
+      })
+      
     })
-    .catch((error)=>
+    .catch(function (error) {
+      console.log(error);
+    });
+    
+  }
+  const change=(e)=>
+  {
+    let b=e.target.files[0];
+    updatedata((datas)=>
     {
-      console.log(error)
+      datas.image=b;
+      return{...datas}
+
     })
   }
+  
   useEffect(()=>
   {
 
   },[])
+  console.log(data.getting)
   return (
     <>
-    <div className='App'>
-  
-    <div > {print}</div>
-   
-    <button onClick={kuku}>click me</button>
-    </div>
+    <img style={{height:"100px",width:"100px"}} src={data.getting} alt=""/>
+    <form method='post' onSubmit={check} encType="multipart/form-data">
+    <input type="file"name="image" onChange={change}/>
+    <button type="submit" className="btn btn-success">Submit</button>
+    <img src="{responses}" style={{height:"100px",weight:"100px"}} alt=""></img>
+  </form>
     </>
   )
 }
